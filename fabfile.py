4bf12035aafe8e08dbf74810550b94a33f0171ad
herereadthis <herereadthis@gmail.com>
2014-10-17 00:11:45 -0400

    raise exceptions for environment variables

--- fabfile.py
+++ fabfile.py
@@ -1,3 +1,4 @@
+import os
 from fabric.api import task, local, lcd
 
 REPO_LIST = [
@@ -19,6 +20,12 @@ REPO_LIST = [
 ]
 
 
+# environment variables
+GITHUB_OWNER = os.environ.get('GITHUB_OWNER', None)
+GITHUB_ACCESS_TOKEN = os.environ.get('GITHUB_ACCESS_TOKEN', None)
+TRAVIS_TOKEN = os.environ.get('TRAVIS_TOKEN', None)
+
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -26,6 +33,24 @@ class bcolors:
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
+
+
+def env_github_owner():
+    if not GITHUB_OWNER:
+        except_statement = "No GITHUB_OWNER in environment variables!"
+        raise Exception(except_statement)
+
+
+def env_github_access_token():
+    if not GITHUB_ACCESS_TOKEN:
+        except_statement = "No GITHUB_ACCESS_TOKEN in environment variables!"
+        raise Exception(except_statement)
+
+
+def env_travis_token():
+    if not TRAVIS_TOKEN:
+        except_statement = "No TRAVIS_TOKEN in environment variables!"
+        raise Exception(except_statement)
 
 
 @task
